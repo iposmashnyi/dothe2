d7267e3842d29ef2ea531755dcdcdb180da8ddd8
Ihor Posmashnyi <iposmashnyi@gmail.com>
2025-05-13 20:06:51 +0200

    move config to a correct path

--- app/core/db/database.py
+++ app/core/db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.ext.asyncio.session import AsyncSession
 from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass, sessionmaker
 
-from .config import settings
+from app.core.config import settings
 
 
 class Base(DeclarativeBase, MappedAsDataclass):
