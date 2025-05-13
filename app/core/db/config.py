37cae2ebe7301bd210ff151d19aa6bf873e31e22
Ihor Posmashnyi <iposmashnyi@gmail.com>
2025-05-13 17:50:46 +0200

    add pydantic-settings lib and config settings

---
+++ app/core/db/config.py
@@ -0,0 +1,55 @@
+import os
+from enum import Enum
+
+from pydantic_settings import BaseSettings
+from starlette.config import Config
+
+current_file_dir = os.path.dirname(os.path.realpath(__file__))
+env_path = os.path.join(current_file_dir, "..", "..", ".env")
+config = Config(env_path)
+
+
+class AppSettings(BaseSettings):
+    APP_NAME: str = config("APP_NAME", default="Dothe2")
+    APP_DESCRIPTION: str | None = config("APP_DESCRIPTION", default=None)
+    APP_VERSION: str | None = config("APP_VERSION", default=None)
+    LICENSE_NAME: str | None = config("LICENSE", default=None)
+    CONTACT_NAME: str | None = config("CONTACT_NAME", default=None)
+    CONTACT_EMAIL: str | None = config("CONTACT_EMAIL", default=None)
+
+
+class DatabaseSettings(BaseSettings):
+    pass
+
+
+class PostgresSettings(DatabaseSettings):
+    POSTGRES_USER: str = config("POSTGRES_USER", default="postgres")
+    POSTGRES_PASSWORD: str = config("POSTGRES_PASSWORD", default="postgres")
+    POSTGRES_SERVER: str = config("POSTGRES_SERVER", default="localhost")
+    POSTGRES_PORT: int = config("POSTGRES_PORT", default=5432)
+    POSTGRES_DB: str = config("POSTGRES_DB", default="postgres")
+    POSTGRES_SYNC_PREFIX: str = config("POSTGRES_SYNC_PREFIX", default="postgresql://")
+    POSTGRES_ASYNC_PREFIX: str = config("POSTGRES_ASYNC_PREFIX", default="postgresql+asyncpg://")
+    POSTGRES_URI: str = f"{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    POSTGRES_URL: str | None = config("POSTGRES_URL", default=None)
+
+
+class EnvironmentOption(Enum):
+    LOCAL = "local"
+    STAGING = "staging"
+    PRODUCTION = "production"
+
+
+class EnvironmentSettings(BaseSettings):
+    ENVIRONMENT: EnvironmentOption = config("ENVIRONMENT", default="local")
+
+
+class Settings(
+    AppSettings,
+    PostgresSettings,
+    EnvironmentSettings,
+):
+    pass
+
+
+settings = Settings()
