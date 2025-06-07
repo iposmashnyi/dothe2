a32feaeb2592d0faa45c8976e7bfc64e94bc97c1
Ihor Posmashnyi <iposmashnyi@gmail.com>
2025-06-07 17:36:28 +0200

    add database dependency;

--- src/app/api/dependencies.py
+++ src/app/api/dependencies.py
@@ -1,6 +1,9 @@
 from typing import Annotated
 
-from fastapi import Header, HTTPException
+from fastapi import Depends, Header, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.db.database import async_get_db
 
 
 async def get_token_header(x_token: Annotated[str, Header()]):
@@ -11,3 +14,11 @@ async def get_token_header(x_token: Annotated[str, Header()]):
 async def get_query_token(token: str):
     if token != "jessica":
         raise HTTPException(status_code=400, detail="No Jessica token provided")
+
+
+async def get_db():
+    async for session in async_get_db():
+        yield session
+
+
+DatabaseDep = Annotated[AsyncSession, Depends(get_db)]
